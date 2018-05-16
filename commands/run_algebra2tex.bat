67e2946812dc99269a91acea3d276bd2caad9240
JohnWCartmell <31049036+JohnWCartmell@users.noreply.github.com>
2018-05-16 11:00:23 +0100

    Just generate tex - nothing else.

--- commands/run_algebra2tex.bat
+++ commands/run_algebra2tex.bat
@@ -1,12 +1,6 @@
 call %~dp0\set_path_variables
 
 set THEORY=%1
+set ITERATION=%2
 
-java -jar %SAXON_PATH%\saxon9he.jar -opt:0 -s:%THEORY%/theory/algebra.xml -xsl:%THEORY%/theory/main_initial_enrichment.xslt -o:%THEORY%/theory/temp/algebra.enriched.xml
-
-java -jar %SAXON_PATH%\saxon9he.jar -s:%THEORY%/theory/temp/algebra.enriched.xml -xsl:algebraLibrary\seqrules2xslt.xslt -o:%THEORY%/theory/temp/rewrite.module.xslt
-
-java -jar %SAXON_PATH%\saxon9he.jar -opt:0 -s:%THEORY%/theory/temp/algebra.enriched.xml -xsl:%THEORY%/theory/main_type_enrichment.xslt -o:%THEORY%/theory/temp/algebra.type_enriched.xml
-
-java -jar %SAXON_PATH%\saxon9he.jar -opt:0 -s:%THEORY%\theory\temp\algebra.type_enriched.xml -xsl:%THEORY%\theory\main.xslt -im:tex_rulestyle -o:%THEORY%/docs/algebra.tex
-
+java -jar %SAXON_PATH%\saxon9he.jar -s:%THEORY%\temp\algebra%ITERATION%.type_enriched.xml -xsl:%THEORY%\theory\main.xslt -im:tex_rulestyle -o:%THEORY%/docs/algebra%ITERATION%.tex
